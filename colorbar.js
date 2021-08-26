8b730edf9037dcbdb6e6dfe6432e177149719d6a
Pan <panzhao12@gmail.com>
2021-08-26 23:35:30 +0200

    now can change the color of olga dynamically

--- colorbar.js
+++ colorbar.js
@@ -1,2 +1,20 @@
-(function() {
-})();
+let colorbar = {
+     elements: {
+          slices: document.querySelectorAll('.slice'),
+     },
+     colorBar: document.getElementById('color-bar'),
+     init: function () {
+          this.colorBar.addEventListener('input', function () {
+               let hue = ((this.colorBar.value / 100) * 360).toFixed(0);
+               let hsl = 'hsl(' + hue + ', 100%, 50%)';
+
+               //change color dynamically
+               this.colorBar.style.color = this.colorBar.value == 0 ? 'black' : hsl;
+               this.elements.slices.forEach(element => {
+                    element.style.background = this.colorBar.value == 0 ? 'black' : hsl;
+               });
+          }.bind(this))
+     }
+}
+
+colorbar.init();
